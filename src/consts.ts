36ec481530e9074ba97c125f6b6446f59e140a52
Omar Facundo <114097196+omar5o9@users.noreply.github.com>
2023-11-15 17:32:14 +0000

    blog

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Soccer Blog';
+export const SITE_DESCRIPTION = 'Website about Mudryk the GOAT!';
---
+++ src/content/blog/soccer-stuff.md
@@ -0,0 +1,6 @@
+---
+title: 'Soccer post'
+description: 'Mudryk is the GOAT'
+pubDate: 'Nov 15 2023'
+heroImage: '/mudryk.jpg'
+---
